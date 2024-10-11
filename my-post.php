8b176d3909382d0da3ff77e04dcd27692a11a47b
Sedenu <aloayecharles@gmail.com>
2024-10-11 16:19:00 +0100

    Update my-post.php

--- my-post.php
+++ my-post.php
@@ -1,25 +1,25 @@
-<?php 
-include('Includes/Header.php'); 
-include('Auth/NotLoggedin.php'); 
+<?php
+include('Includes/Header.php');
+include('Auth/NotLoggedin.php');
 ?>
 <div class="container mt-4">
 
 
-    <?php if($myposts):?>
+    <?php if ($myposts): ?>
 
-<h4> <span  class="badge badge-pills btn-danger"><?= $total_posts; ?>post<?php if($total_posts > 1) { echo 's'; } ?></span> </h4>
+    <h4> <span class="badge badge-pills btn-danger"><?= $total_posts; ?>post<?= $total_posts > 1 ? 's' : '' ?></span> </h4>
 
-    <?php foreach($myposts as $post):?>
+    <?php foreach ($myposts as $post): ?>
     <a href="<?= APP_HOME; ?>post.php?id=<?= $post->p_id; ?>">
         <div class="card mb-2 post">
             <div class="card-body">
                 <h4 class="card-title"><?= $post->title; ?></h4>
-                <small>Written by: <?= $post->name; ?> | <?= $post->created_at;?></small>
+                <small>Written by: <?= $post->name; ?> | <?= $post->created_at; ?></small>
             </div>
         </div>
     </a>
 
-    <?php endforeach;?>
+    <?php endforeach; ?>
 
     <?php else: ?>
 
@@ -29,5 +29,5 @@ include('Auth/NotLoggedin.php');
         <a href="<?= APP_HOME; ?>new-post.php" class="btn btn-outline-light">New Post</a>
     </div>
 
-    <?php endif;?>
+    <?php endif; ?>
 </div>
