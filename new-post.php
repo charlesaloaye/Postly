c65181f91119b1848b586ae8e8f98e4bec174979
Sedenu <aloayecharles@gmail.com>
2024-10-11 16:19:19 +0100

    Update new-post.php

--- new-post.php
+++ new-post.php
@@ -1,32 +1,32 @@
-<?php 
-include('Includes/Header.php'); 
-include('Auth/NotLoggedin.php'); 
+<?php
+include('Includes/Header.php');
+include('Auth/NotLoggedin.php');
 ?>
 <div class="card shadow p-3 mt-5">
-<form action="" method="post" enctype="multipart/form-data">
-<h1>New Post</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <h1>New Post</h1>
 
-<div class="alert <?= $status; ?>">
-<?= $message; ?>
-</div>
+        <div class="alert <?= $status; ?>">
+            <?= $message; ?>
+        </div>
 
-<div class="form-group">
-    <input accept="image/*" type="file" name="post_image" id="" class="form-control">
-</div>
+        <div class="form-group">
+            <input accept="image/*" type="file" name="post_image" id="" class="form-control">
+        </div>
 
-<div class="form-group mt-3">
-        <input type="text" name="title" placeholder="Enter Title" class="form-control" value="<?= $title; ?>">
-    </div>
+        <div class="form-group mt-3">
+            <input type="text" name="title" placeholder="Enter Title" class="form-control" value="<?= $title; ?>">
+        </div>
 
 
-    <div class="form-group mt-3 mb-3">
-       <textarea name="body" placeholder="Body" rows="10" class="form-control" ></textarea>
-    </div>
+        <div class="form-group mt-3 mb-3">
+            <textarea name="body" placeholder="Body" rows="10" class="form-control"></textarea>
+        </div>
 
-    <div class="form-group">
-        <button type="submit" class="btn btn-danger" name='add_post'>Add New Post &rarr;</button>
-    </div>
-</form>
+        <div class="form-group">
+            <button type="submit" class="btn btn-danger" name='add_post'>Add New Post &rarr;</button>
+        </div>
+    </form>
 </div>
 
 
