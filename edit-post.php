8d6c876b5fab5437b0555ec32cf442dc7ee34119
Sedenu <aloayecharles@gmail.com>
2024-10-11 16:17:02 +0100

    Update edit-post.php

--- edit-post.php
+++ edit-post.php
@@ -1,41 +1,38 @@
-<?php 
-include('Includes/Header.php'); 
-include('Auth/NotLoggedin.php'); 
+<?php
+include('Includes/Header.php');
+include('Auth/NotLoggedin.php');
 
-// if($post->uid != $user->uid){
-//     header('location: '. APP_HOME);
-// }
 ?>
 <div class="card shadow p-3 mt-5">
-<form action="" method="post">
-<h1><?= $page_name; ?></h1>
+    <form action="" method="post">
+        <h1><?= $page_name; ?></h1>
 
-<div class="alert <?= $status; ?>">
-<?= $message; ?>
-</div>
+        <div class="alert <?= $status; ?>">
+            <?= $message; ?>
+        </div>
 
 
-<?php if($post->uid == $user->uid):?>
+        <?php if ($post->uid == $user->uid): ?>
 
-<div class="form-group mt-3">
-        <input type="text" name="title" placeholder="Enter Title" class="form-control" value="<?= $post->title; ?>">
+        <div class="form-group mt-3">
+            <input type="text" name="title" placeholder="Enter Title" class="form-control" value="<?= $post->title; ?>">
+        </div>
+
+
+        <div class="form-group mt-3 mb-3">
+            <textarea name="body" placeholder="Body" rows="10" class="form-control"><?= $post->body; ?></textarea>
+        </div>
+        <input type="hidden" name="p_id" value='<?= $post->p_id; ?>'>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-danger" name='update_post'>Update Post &rarr;</button>
+        </div>
+    </form>
+    <?php else : ?>
+
+    <div class="jumbotron p-3 bg-danger">
+        <h2 class='text-light'>You don't have access to this resource, please contact the post author.</h2>
     </div>
-
-
-    <div class="form-group mt-3 mb-3">
-       <textarea name="body" placeholder="Body" rows="10" class="form-control" ><?= $post->body; ?></textarea>
-    </div>
-    <input type="hidden" name="p_id" value='<?= $post->p_id; ?>'>
-
-    <div class="form-group">
-        <button type="submit" class="btn btn-danger" name='update_post'>Update Post &rarr;</button>
-    </div>
-</form>
-<?php else :?>
-
-<div class="jumbotron p-3 bg-danger">
-    <h2 class='text-light'>You don't have access to this resource, please contact the post author.</h2>
-</div>
 
     <?php endif; ?>
 </div>
